00828c21beb7035fe8f928375d2d73d5739091aa
Paul K <pa179kin@gmail.com>
2022-08-06 13:07:20 +0200

    cleanup

--- lib/src/main/kotlin/me/kxmpxtxnt/chess/Chess.kt
+++ lib/src/main/kotlin/me/kxmpxtxnt/chess/Chess.kt
@@ -4,13 +4,5 @@ import me.kxmpxtxnt.chess.board.ChessBoard
 import me.kxmpxtxnt.chess.fen.save.lineup.LineupSaver
 
 fun main() {
-  val board = ChessBoard(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")
-
-  with(board.lineup.keys.sortedBy { it.fieldID }) {
-    this.forEach {
-      println("|| $it || ${board.lineup[it]}")
-    }
-  }
-
-  println(LineupSaver.save(board))
+  ChessBoard(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 0")
 }
